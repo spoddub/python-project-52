6903c97264c5d0ffc1cb33226d74d82cdabb9c33
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2025-09-23 17:03:36 +0300

    add task

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class Status(models.Model):
@@ -9,4 +12,23 @@ class Status(models.Model):
         ordering = ["id"]
 
     def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=255, unique=False)
+    description = models.TextField(blank=True)
+    status = models.ForeignKey(Status, on_delete=models.PROTECT, related_name="tasks")
+    author = models.ForeignKey(
+        User, on_delete=models.PROTECT, related_name="authored_tasks", editable=False
+    )
+    executor = models.ForeignKey(
+        User, on_delete=models.PROTECT, related_name="assigned_tasks", null=True, blank=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["id"]
+
+    def __str__(self) -> str:
         return self.name
