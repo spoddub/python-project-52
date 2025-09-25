20cb0b38040672afa03e4b5876d7e480e081bcdc
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2025-09-25 21:27:54 +0300

    update url

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -15,37 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.conf import settings
 from django.contrib import admin
-from django.urls import path
-
-from . import views
+from django.urls import include, path
 
 urlpatterns = [
-    path("", views.index, name="home"),
+    path("", include("task_manager.apps.core.urls")),
     path("admin/", admin.site.urls),
-    path("login/", views.SignInView.as_view(), name="login"),
-    path("logout/", views.LogoutPostOnlyView.as_view(), name="logout"),
-    path("users/", views.UsersListView.as_view(), name="users_list"),
-    path("users/create/", views.UserCreateView.as_view(), name="users_create"),
-    path("users/<int:pk>/update/", views.UserUpdateView.as_view(), name="users_update"),
-    path("users/<int:pk>/delete/", views.UserDeleteView.as_view(), name="users_delete"),
-    path("statuses/", views.StatusListView.as_view(), name="statuses_list"),
-    path("statuses/create/", views.StatusCreateView.as_view(), name="statuses_create"),
-    path("statuses/<int:pk>/update/", views.StatusUpdateView.as_view(), name="statuses_update"),
-    path("statuses/<int:pk>/delete/", views.StatusDeleteView.as_view(), name="statuses_delete"),
-    path("tasks/", views.TasksListView.as_view(), name="tasks_list"),
-    path("tasks/create/", views.TaskCreateView.as_view(), name="tasks_create"),
-    path("tasks/<int:pk>/", views.TaskDetailView.as_view(), name="tasks_detail"),
-    path("tasks/<int:pk>/update/", views.TaskUpdateView.as_view(), name="tasks_update"),
-    path("tasks/<int:pk>/delete/", views.TaskDeleteView.as_view(), name="tasks_delete"),
-    path("labels/", views.LabelsListView.as_view(), name="labels_list"),
-    path("labels/create/", views.LabelCreateView.as_view(), name="labels_create"),
-    path("labels/<int:pk>/update/", views.LabelUpdateView.as_view(), name="labels_update"),
-    path("labels/<int:pk>/delete/", views.LabelDeleteView.as_view(), name="labels_delete"),
+    path("users/", include("task_manager.apps.users.urls")),
+    path("statuses/", include("task_manager.apps.statuses.urls")),
+    path("tasks/", include("task_manager.apps.tasks.urls")),
+    path("labels/", include("task_manager.apps.labels.urls")),
 ]
-
-if settings.DEBUG:
-    urlpatterns += [
-        path("rollbar-test/", views.rollbar_test_view, name="rollbar_test"),
-    ]
